1410bbdd3cc7c7009cfd8c747af4478b3e0e431f
Justin Wen-Lo Yang <justinwenloyang@gmail.com>
2015-03-22 21:00:15 +0000

    update

---
+++ ultrasonic_HCSR04_wFilter/ultrasonic_HCSR04_wFilter.ino
@@ -0,0 +1,139 @@
+/*
+ * created by Rui Santos, http://randomnerdtutorials.com
+ *
+ * Complete Guide for Ultrasonic Sensor HC-SR04
+ *
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+
+This script is designed to take several readings from the maxbotix sonar and generate a mode/median.
+Author: Jason Lessels
+Date created: 2011/June/06
+Lisence: GPL (=>2)
+This work has been compileed using many sources mainly posts/wiki posts from;
+Allen, Bruce (2009/July/23) and Gentles, Bill (2010/Nov/12)
+
+Modified by Justin Yang 2015
+*/
+
+int trigPin = 11;    //Trig - green Jumper
+int echoPin = 12;    //Echo - yellow Jumper
+long duration, cm;
+
+int arraysize = 15; //quantity of values to find the median (sample size). Needs to be an odd number
+
+//declare an array to store the samples. not necessary to zero the array values here, it just makes the code clearer
+int rangevalue[15];
+int modE;
+
+void setup() {
+  //Serial Port begin
+  Serial.begin (115200);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  
+  for (int i = 0; i < arraysize; i++) {
+    // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Read the signal from the sensor: a HIGH pulse whose
+    // duration is the time (in microseconds) from the sending
+    // of the ping to the reception of its echo off of an object.
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+    // convert the time into a distance
+    cm = (duration / 2) / 29.1;
+    rangevalue[i] = cm;
+    delay(1);
+  }
+
+  isort(rangevalue, arraysize);
+  modE = mode(rangevalue, arraysize);
+  if(modE<65){ // To eliminate outliers, can adjust this number to a greater range
+  Serial.print("Us1:");
+  Serial.print(modE); //the mode filtered value
+  Serial.print(";");
+  }
+
+  
+  
+  Serial.println();
+
+  delay(15);
+}
+
+
+
+
+
+/*-----------Functions------------*/ //Function to print the arrays.
+void printArray(int *a, int n) {
+
+  for (int i = 0; i < n; i++)
+  {
+    Serial.print(a[i], DEC);
+    Serial.print(' ');
+  }
+  Serial.println();
+}
+
+//Sorting function
+// sort function (Author: Bill Gentles, Nov. 12, 2010)
+void isort(int *a, int n) {
+  // *a is an array pointer function
+  for (int i = 1; i < n; ++i)
+  {
+    int j = a[i];
+    int k;
+    for (k = i - 1; (k >= 0) && (j < a[k]); k--)
+    {
+      a[k + 1] = a[k];
+    }
+    a[k + 1] = j;
+  }
+}
+
+//Mode function, returning the mode or median.
+int mode(int *x, int n) {
+
+  int i = 0;
+  int count = 0;
+  int maxCount = 0;
+  int mode = 0;
+  int bimodal;
+  int prevCount = 0;
+  while (i < (n - 1)) {
+    prevCount = count;
+    count = 0;
+    while (x[i] == x[i + 1]) {
+      count++;
+      i++;
+    }
+    if (count > prevCount & count > maxCount) {
+      mode = x[i];
+      maxCount = count;
+      bimodal = 0;
+    }
+    if (count == 0) {
+      i++;
+    }
+    if (count == maxCount) { //If the dataset has 2 or more modes.
+      bimodal = 1;
+    }
+    if (mode == 0 || bimodal == 1) { //Return the median if there is no mode.
+      mode = x[(n / 2)];
+    }
+    return mode;
+  }
+}
